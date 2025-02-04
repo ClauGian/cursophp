4ea48136adca1c2b30cd1aa68f41f48b792cace7
ClauGian <cgclaugian@gmail.com>
2025-02-04 08:02:54 -0300

    Strings

---
+++ exe003/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>  
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+
+    <?php 
+        // $num = 010;
+        // print "O valor da variável é: $num <br>";
+
+        // $v = "Claudinei";
+        // var_dump($v);
+        // print "<br>";
+
+        // $n = 3e2;
+        // var_dump($n); 
+        // print "<br>";
+
+        // $n = (int) 3e2; //Faço a coerção para números inteiros lembrando que potência é sempre float
+        // var_dump($n);
+        
+        // print "PHP \u{1f418}<br>";
+        // print 'PHP \u{1f418}';
+
+        // $nome = "Claudinei";
+        // $sobrenome = "Gianini";
+        // print "$nome $sobrenome \u{1f596}<br>";
+
+        // print '$nome $sobrenome \u{1f596}';
+
+        // const ESTADO = "SP";
+        // print "Moro no estado de ESTADO<br>";
+        
+        // print 'Moro no estado de ESTADO<br>';
+
+        // print "Moro no estado de " . ESTADO;
+
+        //print "Estamos no ano de: " . date('Y.');
+
+        
+
+
+    ?>
+</body>
+</html>
